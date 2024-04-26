8e337d8bd85a83e3ecdc332c6e7b5f0913bb598a
Felipe Santos <nfelipe.ps@gmail.com>
2024-04-26 17:58:53 -0300

    feat: add initial version of Go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,8 @@ func routes(_ *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
