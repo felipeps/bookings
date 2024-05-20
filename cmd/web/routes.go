1e314b79103b708f9d5507c7902c9fccb545322b
Felipe Santos <nfelipe.ps@gmail.com>
2024-05-20 20:48:23 -0300

    feat: ensure make reservation form is validated

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,6 +25,7 @@ func routes(_ *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
